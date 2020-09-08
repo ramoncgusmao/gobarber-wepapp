095e450eb6ee3b064b40a88f9c8ae37357e8b721
ramon gusmao <ramoncgusmao@gmail.com>
2020-09-07 22:21:14 -0300

    criando o header do dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,39 @@
 import React from 'react';
+import {Container, Header, HeaderContent, Profile} from './styles';
+import logoImg from '../../assets/logo.svg';
+import { FiPower } from 'react-icons/fi';
+import { useAuth } from '../../hooks/Auth';
+const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+  console.log(user);
+  return(
+        <Container>
+          <Header>
+            <HeaderContent>
+              <img src={logoImg} alt="GoBarber" />
 
-const Dashboard: React.FC = () => (<h1>DashBoard</h1>);
+              <Profile>
+                <img
+                src={user.avatar_url}
+                alt={user.name}
+                />
+                <div>
+                  <span>Bem-vindo,</span>
+                  <strong>{user.name}</strong>
+                </div>
+              </Profile>
+
+              <button type="button" onClick={signOut}>
+                <FiPower />
+              </button>
+            </HeaderContent>
+
+
+          </Header>
+        </Container>
+  );
+
+};
 
 
 export default Dashboard;
---
+++ src/pages/ResetPassword/index.tsx
@@ -0,0 +1,93 @@
+import React, { useCallback, useRef } from 'react';
+import { Container, Content, Background, AnimationContainer } from './styles';
+import {  FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import logoImg from '../../assets/logo.svg';
+import { Form } from '@unform/web';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
+import { useToast } from '../../hooks/Toast';
+import {  useHistory, useLocation } from 'react-router-dom';
+import api from '../../services/api';
+interface ResetPasswordFormData {
+  password: string;
+  password_confirmation: string;
+}
+
+const SignIn: React.FC = () => {
+
+  const formRef = useRef<FormHandles>(null);
+
+  const { addToast } = useToast();
+  const history = useHistory();
+  const location = useLocation();
+  const handleSubmit = useCallback(async (data: ResetPasswordFormData) => {
+    try {
+
+      formRef.current?.setErrors([]);
+      const schema = Yup.object().shape({
+        password: Yup.string().required("senha obrigatoria"),
+        password_confirmation: Yup.string().oneOf([Yup.ref('password')], 'as senhas precisam ser iguais')
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      const {password, password_confirmation} = data;
+      const token = location.search.split('=').pop();
+
+      if(!token){
+       throw new Error();
+        
+      }
+
+      await api.post('/password/reset', {
+        password,
+        password_confirmation,
+        token,
+      });
+
+      history.push('/');
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+      } else {
+        addToast({
+          type: 'error',
+          title: 'Erro ao resetar senha',
+          description: 'Ocorreu um erro ao resetar sua senha, tente novamente',
+        });
+      }
+
+    }
+
+  }, [ addToast, history]);
+
+  return (
+    <Container>
+      <Content>
+        <AnimationContainer>
+
+          <img src={logoImg} alt="GoBarber" />
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Resetar senha</h1>
+           
+            <Input name="password" icon={FiLock} type="password" placeholder="Nova senha" />
+            <Input name="password_confirmation" icon={FiLock} type="password" placeholder="Confirmação da senha" />
+           
+            <Button type="submit">Alterar senha</Button>
+          </Form>
+
+        
+        </AnimationContainer>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default SignIn;
